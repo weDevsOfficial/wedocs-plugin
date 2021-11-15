4b5adad8c199303d2c95990056366df6aaae6e01
Tareq Hasan <tareq1988@gmail.com>
2021-11-15 08:50:46 +0300

    Sweetalert library updated. Fixes #123

--- assets/js/admin-script.js
+++ assets/js/admin-script.js
@@ -5,249 +5,273 @@
 /* global swal */
 /* global ajaxurl */
 
-Vue.directive('sortable', {
-    bind: function(el, binding) {
+Vue.directive("sortable", {
+    bind: function (el, binding) {
         var $el = jQuery(el);
 
         $el.sortable({
-            handle: '.wedocs-btn-reorder',
-            stop: function(event, ui) {
+            handle: ".wedocs-btn-reorder",
+            stop: function (event, ui) {
                 var ids = [];
 
-                jQuery( ui.item.closest('ul') ).children('li').each(function(index, el) {
-                    ids.push( jQuery(el).data('id'));
-                });
+                jQuery(ui.item.closest("ul"))
+                    .children("li")
+                    .each(function (index, el) {
+                        ids.push(jQuery(el).data("id"));
+                    });
 
                 wp.ajax.post({
-                    action: 'wedocs_sortable_docs',
+                    action: "wedocs_sortable_docs",
                     ids: ids,
-                    _wpnonce: weDocs.nonce
+                    _wpnonce: weDocs.nonce,
                 });
             },
-            cursor: 'move',
+            cursor: "move",
             // connectWith: ".connectedSortable"
         });
-    }
+    },
 });
 
 new Vue({
-    el: '#wedocs-app',
+    el: "#wedocs-app",
     data: {
-        editurl: '',
-        viewurl: '',
-        docs: []
+        editurl: "",
+        viewurl: "",
+        docs: [],
     },
 
-    mounted: function() {
+    mounted: function () {
         var self = this,
-            dom = jQuery( self.$el );
+            dom = jQuery(self.$el);
 
         this.editurl = weDocs.editurl;
         this.viewurl = weDocs.viewurl;
 
-        dom.find('ul.docs').removeClass('not-loaded').addClass('loaded');
+        dom.find("ul.docs").removeClass("not-loaded").addClass("loaded");
 
-        jQuery.get(ajaxurl, {
-            action: 'wedocs_admin_get_docs',
-            _wpnonce: weDocs.nonce
-        }, function(data) {
-            dom.find('.spinner').remove();
-            dom.find('.no-docs').removeClass('not-loaded');
+        jQuery.get(
+            ajaxurl,
+            {
+                action: "wedocs_admin_get_docs",
+                _wpnonce: weDocs.nonce,
+            },
+            function (data) {
+                dom.find(".spinner").remove();
+                dom.find(".no-docs").removeClass("not-loaded");
 
-            self.docs = data.data;
-        });
+                self.docs = data.data;
+            }
+        );
     },
 
     methods: {
-
-        onError: function(error) {
+        onError: function (error) {
             alert(error);
         },
 
-        addDoc: function() {
+        swalInputValidator: function (value) {
+            if (!value) {
+                return "You need to write something";
+            }
+        },
 
+        addDoc: function () {
             var that = this;
             this.docs = this.docs || [];
 
-            swal({
+            Swal.fire({
                 title: weDocs.enter_doc_title,
-                type: "input",
+                input: "text",
                 showCancelButton: true,
-                closeOnConfirm: true,
-                animation: "slide-from-top",
                 inputPlaceholder: weDocs.write_something,
                 confirmButtonText: weDocs.confirmBtn,
-                cancelButtonText: weDocs.cancelBtn
-            }, function(inputValue){
-                if (inputValue === false) {
-                    return false;
+                cancelButtonText: weDocs.cancelBtn,
+                inputValidator: this.swalInputValidator,
+            }).then(function (input) {
+                if (input.isDismissed) {
+                    return;
                 }
 
-                wp.ajax.send( {
+                wp.ajax.send({
                     data: {
-                        action: 'wedocs_create_doc',
-                        title: inputValue,
+                        action: "wedocs_create_doc",
+                        title: input.value.trim(),
                         parent: 0,
-                        _wpnonce: weDocs.nonce
+                        _wpnonce: weDocs.nonce,
                     },
-                    success: function(res) {
-                        that.docs.unshift( res );
+                    success: function (res) {
+                        that.docs.unshift(res);
                     },
-                    error: this.onError
+                    error: this.onError,
                 });
-
             });
         },
 
-        removeDoc: function(doc, docs) {
+        removeDoc: function (doc, docs) {
             var self = this;
 
-            swal({
+            Swal.fire({
                 title: "Are you sure?",
                 text: "Are you sure to delete the entire documentation? Sections and articles inside this doc will be deleted too!",
-                type: "warning",
+                icon: "warning",
                 showCancelButton: true,
                 confirmButtonColor: "#DD6B55",
                 confirmButtonText: weDocs.delConfirmBtn,
-                closeOnConfirm: false,
-                cancelButtonText: weDocs.cancelBtn
-            }, function() {
-                self.removePost(doc, docs);
+                cancelButtonText: weDocs.cancelBtn,
+            }).then(function (action) {
+                if (action.isConfirmed) {
+                    self.removePost(doc, docs, "The doc has been deleted.");
+                }
             });
         },
 
-        addSection: function(doc) {
-            swal({
+        addSection: function (doc) {
+            Swal.fire({
                 title: weDocs.enter_section_title,
-                type: "input",
+                input: "text",
                 showCancelButton: true,
-                closeOnConfirm: true,
-                animation: "slide-from-top",
+                confirmButtonColor: "#2271b1",
                 inputPlaceholder: weDocs.write_something,
                 confirmButtonText: weDocs.confirmBtn,
-                cancelButtonText: weDocs.cancelBtn
-            }, function(inputValue){
-                if (inputValue === false) {
-                    return false;
+                cancelButtonText: weDocs.cancelBtn,
+                inputValidator: this.swalInputValidator,
+            }).then(function (input) {
+                if (input.isDismissed) {
+                    return;
                 }
 
-                inputValue = inputValue.trim();
-
-                if ( inputValue ) {
-                    wp.ajax.send( {
+                if (input.value) {
+                    wp.ajax.send({
                         data: {
-                            action: 'wedocs_create_doc',
-                            title: inputValue,
+                            action: "wedocs_create_doc",
+                            title: input.value.trim(),
                             parent: doc.post.id,
                             order: doc.child.length,
-                            _wpnonce: weDocs.nonce
+                            _wpnonce: weDocs.nonce,
                         },
-                        success: function(res) {
-                            doc.child.push( res );
+                        success: function (res) {
+                            doc.child.push(res);
                         },
-                        error: this.onError
+                        error: this.onError,
                     });
                 }
             });
         },
 
-        removeSection: function(section, sections) {
+        removeSection: function (section, sections) {
             var self = this;
 
-            swal({
+            Swal.fire({
                 title: weDocs.delConfirm,
                 text: weDocs.delConfirmTxt,
-                type: "warning",
+                icon: "warning",
                 showCancelButton: true,
                 confirmButtonColor: "#DD6B55",
                 confirmButtonText: weDocs.delConfirmBtn,
                 cancelButtonText: weDocs.cancelBtn,
-                closeOnConfirm: false
-            }, function() {
-                self.removePost(section, sections);
+            }).then(function (action) {
+                if (action.isDismissed) {
+                    return;
+                }
+
+                if (action.isConfirmed) {
+                    self.removePost(section, sections);
+                }
             });
         },
 
-        addArticle: function(section, event) {
+        addArticle: function (section, event) {
             var parentEvent = event;
 
-            swal({
+            Swal.fire({
                 title: weDocs.enter_doc_title,
-                type: "input",
+                input: "text",
                 showCancelButton: true,
-                closeOnConfirm: true,
-                animation: "slide-from-top",
                 inputPlaceholder: weDocs.write_something,
                 confirmButtonText: weDocs.confirmBtn,
-                cancelButtonText: weDocs.cancelBtn
-            }, function(inputValue){
-                if (inputValue === false) {
+                cancelButtonText: weDocs.cancelBtn,
+                inputValidator: this.swalInputValidator,
+            }).then(function (input) {
+                if (input.isDismissed) {
+                    return;
+                }
+
+                if (input.value === false || input.value === "") {
                     return false;
                 }
 
-                wp.ajax.send( {
+                wp.ajax.send({
                     data: {
-                        action: 'wedocs_create_doc',
-                        title: inputValue,
+                        action: "wedocs_create_doc",
+                        title: input.value,
                         parent: section.post.id,
-                        status: 'draft',
+                        status: "draft",
                         order: section.child.length,
-                        _wpnonce: weDocs.nonce
+                        _wpnonce: weDocs.nonce,
                     },
-                    success: function(res) {
-                        section.child.push( res );
+                    success: function (res) {
+                        section.child.push(res);
 
-                        var articles = jQuery( parentEvent.target ).closest('.section-title').next();
+                        var articles = jQuery(parentEvent.target)
+                            .closest(".section-title")
+                            .next();
 
-                        if ( articles.hasClass('collapsed') ) {
-                            articles.removeClass('collapsed');
+                        if (articles.hasClass("collapsed")) {
+                            articles.removeClass("collapsed");
                         }
                     },
-                    error: function(error) {
-                        alert( error );
-                    }
+                    error: function (error) {
+                        alert(error);
+                    },
                 });
             });
         },
 
-        removeArticle: function(article, articles) {
+        removeArticle: function (article, articles) {
             var self = this;
 
-            swal({
+            Swal.fire({
                 title: weDocs.delConfirm,
                 text: weDocs.delConfirmTxt,
-                type: "warning",
+                icon: "warning",
                 showCancelButton: true,
                 confirmButtonColor: "#DD6B55",
                 confirmButtonText: weDocs.delConfirmBtn,
                 cancelButtonText: weDocs.cancelBtn,
-                closeOnConfirm: false
-            }, function(){
-                self.removePost(article, articles);
-            });
-        },
-
-        removePost: function(index, items, message) {
-            message = message || 'This post has been deleted';
-
-            wp.ajax.send( {
-                data: {
-                    action: 'wedocs_remove_doc',
-                    id: items[index].post.id,
-                    _wpnonce: weDocs.nonce
-                },
-                success: function() {
-                    Vue.delete(items, index);
-                    swal( 'Deleted!', message, 'success' );
-                },
-                error: function(error) {
-                    alert( error );
+            }).then(function (action) {
+                if (action.isConfirmed) {
+                    self.removePost(article, articles);
                 }
             });
         },
 
-        toggleCollapse: function(event) {
-            jQuery(event.target).siblings('ul.articles').toggleClass('collapsed');
-        }
+        removePost: function (index, items, message) {
+            message = message || "This post has been deleted";
+
+            wp.ajax.send({
+                data: {
+                    action: "wedocs_remove_doc",
+                    id: items[index].post.id,
+                    _wpnonce: weDocs.nonce,
+                },
+                success: function () {
+                    Vue.delete(items, index);
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: message,
+                        icon: "success",
+                    });
+                },
+                error: function (error) {
+                    alert(error);
+                },
+            });
+        },
+
+        toggleCollapse: function (event) {
+            jQuery(event.target)
+                .siblings("ul.articles")
+                .toggleClass("collapsed");
+        },
     },
 });
