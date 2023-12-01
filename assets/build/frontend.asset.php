7148f6dba4ee82a43c0c284fc41dfcb94ea13f55
MdAsifHossainNadim <nadim@wedevs.com>
2023-12-01 16:43:23 +0600

    fix: documentation-archive-page-section-article-title-truncted-issue

--- assets/build/frontend.asset.php
+++ assets/build/frontend.asset.php
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 <?php return array('dependencies' => array(), 'version' => '0022b05841da55c0df98');
+=======
+<?php return array('dependencies' => array(), 'version' => 'f16c62340637dc2329b3');
+>>>>>>> Stashed changes
