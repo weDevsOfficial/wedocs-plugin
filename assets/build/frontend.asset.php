8f79e4b01e91c985bcd23f5127ebeac6d072596a
MdAsifHossainNadim <nadim@wedevs.com>
2023-12-05 12:11:34 +0600

    insert: add-editor-role-insertion-build-file

--- assets/build/frontend.asset.php
+++ assets/build/frontend.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< Updated upstream
 <?php return array('dependencies' => array(), 'version' => '0022b05841da55c0df98');
-=======
-<?php return array('dependencies' => array(), 'version' => '4c2913edd02fa534bad9');
->>>>>>> Stashed changes
