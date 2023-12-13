7bbc28310eab227d9308dd23c503461c741b4abc
MdAsifHossainNadim <nadim@wedevs.com>
2023-12-13 10:17:23 +0600

    enhance: implement-switch-to-draft-action

--- assets/build/index.asset.php
+++ assets/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ba0b9fa551f92cf51f38');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6e6eff71d8244bc17e8b');
