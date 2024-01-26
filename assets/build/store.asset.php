845568aa3e6e70c250af4395fdaa26b510e142f4
Md. Asif Hossain Nadim <90011088+MdAsifHossainNadim@users.noreply.github.com>
2024-01-26 16:19:46 +0600

    enhance/add-article-children-list-in-docs-listing-page (#188)
    * enhance: add-article-children-list-in-docs-listing-page
    
    * enhance: add-draggable-option-for-article-children
    
    * enhance: add-wedocs-content-extraction-function-&-update-children-getter-function
    
    * release: add-releasing-logs-for-version-2-1-2

--- assets/build/store.asset.php
+++ assets/build/store.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-data'), 'version' => '863cd3b694eb2bbe53cc');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-data'), 'version' => 'c40d82727744220dee3f');
