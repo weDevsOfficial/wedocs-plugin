cc2a63a2e990e84546e6d3741b60ea98a65067b4
MdAsifHossainNadim <nadim@wedevs.com>
2024-02-22 09:36:17 +0600

    update: add-search-compiled-data

--- assets/build/block.asset.php
+++ assets/build/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd05819f3aa7353dc2183');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '579b45b72357301838e7');
