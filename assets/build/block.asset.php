f532bff93cda4dd0cceaf745c7097715b314b735
MdAsifHossainNadim <nadim@wedevs.com>
2024-02-27 19:31:54 +0600

    fix: deprecated-error-for-appsero-updater

---
+++ assets/build/block.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '3b523d2a17f959070428');
--- assets/build/index.asset.php
+++ assets/build/index.asset.php
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 <?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '019b62242c90b1fbaead');
+=======
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6b1082601ef3ed3802b6');
+>>>>>>> Stashed changes
