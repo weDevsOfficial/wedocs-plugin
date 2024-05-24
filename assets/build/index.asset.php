3ffbe17c5cc44b3faa1f3d04458e4c3878378f28
Md. Asif Hossain Nadim <90011088+MdAsifHossainNadim@users.noreply.github.com>
2024-05-24 09:13:22 +0600

    feat/documentation-page-search-block-implementation (#191)
    * enhance: add-controls-for-search-block
    
    * enhance: add-block-files
    
    * fix/doc-migration-reactivity-isn't working-perfectly (#190)
    
    * fix: revamp-docs-delete-api-&-fix-deletion-permission-error
    
    * fix: migrated-docs-reactivity-issue
    
    * revert: remove-redunded-codes
    
    * fix: revamp-docs-delete-api-&-fix-deletion-permission-error (#189)
    
    * enhance/add-article-children-list-in-docs-listing-page (#188)
    
    * enhance: add-article-children-list-in-docs-listing-page
    
    * enhance: add-draggable-option-for-article-children
    
    * enhance: add-wedocs-content-extraction-function-&-update-children-getter-function
    
    * release: add-releasing-logs-for-version-2-1-2
    
    * fix: releasing-candidate-for-v2.1.2
    
    * fix: releasing-candidate-for-v2-1-2-&-update-plugin-readme-with-screenshots
    
    * Update main.yml
    
    * Update readme.txt
    
    * Update main.yml
    
    * Update readme.txt
    
    * Update main.yml
    
    * Update readme.txt
    
    * Update main.yml
    
    * release: update-appsero-sdk-&-update-releasing-v2.1.3
    
    * update: placeholder-content-for-search-bar
    
    * enhance: add-block-controls-&-make-it-dynamic-in-editor-panel
    
    * enhance: fix-search-block-releasing-candidates
    
    * enhance: fix-search-block-releasing-candidates
    
    * enhance: add-css-root-before-tailwind-&-make-headless-dialog-compatible
    
    * fix: make-compatible-search-block-with-block-theme
    
    * enhance: update-search-toggle-default-status
    
    * fix: button-hover-state-update
    
    * update: add-search-compiled-data
    
    ---------
    
    Co-authored-by: Saif Hassan <saiftheboss7@gmail.com>

--- assets/build/index.asset.php
+++ assets/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7c658741aacc01511b26');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f4549ed1d2bd8e2e29a6');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: '.wedocs-document',
   content: [ './templates/*.php', './templates/**/*.php', './src/**/*' ],
   theme: {
     extend: {},
