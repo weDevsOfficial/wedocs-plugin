39882314499f1078c30c54a2553374eaa76b61ae
Md. Asif Hossain Nadim <90011088+MdAsifHossainNadim@users.noreply.github.com>
2024-05-24 09:17:29 +0600

    Fix: weDocs dashboard ui conflicts with elementor (#196)

--- assets/build/index.asset.php
+++ assets/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f4549ed1d2bd8e2e29a6');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '14f980f35d0edbdf5927');
