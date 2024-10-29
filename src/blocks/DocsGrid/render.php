9ab4812b621cdcd4878ec90cbd72580c1ee22859
ratulhasan <ratuljh@gmail.com>
2024-10-29 10:41:20 +0600

    Show on frontend

--- src/blocks/DocsGrid/render.php
+++ src/blocks/DocsGrid/render.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Render the docs grid block on frontend with optimized pagination
+ *
+ * @param array  $attributes Block attributes
+ * @param string $content    Block content
+ * @return string Rendered block content
+ */
+function render_wedocs_docs_grid($attributes) {
     // Extract attributes with defaults
     $doc_style = $attributes['docStyle'] ?? '1x1';
     $docs_per_page = $attributes['docsPerPage'] ?? 'all';
@@ -14,30 +22,29 @@
     // Get current page for pagination
     $current_page = max(1, get_query_var('paged'));
 
-    // Query docs
+    // Set up the main docs query
     $args = array(
         'post_type' => 'docs',
         'post_status' => 'publish',
-        'posts_per_page' => -1,
         'post_parent' => 0,
         'orderby' => $order_by,
         'order' => 'ASC',
         'post__not_in' => array_map('intval', $exclude_docs)
     );
 
-    $docs_query = new WP_Query($args);
-    $all_docs = $docs_query->posts;
-
-    // Process pagination
+    // Handle pagination
     if ($enable_pagination && $docs_per_page !== 'all') {
-        $per_page = intval($docs_per_page);
-        $total_pages = ceil(count($all_docs) / $per_page);
-        $offset = ($current_page - 1) * $per_page;
-        $docs = array_slice($all_docs, $offset, $per_page);
+        $args['posts_per_page'] = intval($docs_per_page);
+        $args['paged'] = $current_page;
     } else {
-        $docs = $all_docs;
-        $total_pages = 1;
+        $args['posts_per_page'] = '-1';
+        $args['paged'] = 1;
     }
+
+    // Query docs with pagination
+    $docs_query = new WP_Query($args);
+    $docs = $docs_query->posts;
+    $total_pages = $docs_query->max_num_pages;
 
     // Start output buffering
     ob_start();
@@ -45,17 +52,17 @@
     <div class="wedocs-block-wrapper">
         <div class="wedocs-docs-grid wedocs-docs-grid--<?php echo esc_attr($doc_style); ?>">
             <?php foreach ($docs as $doc) :
-                // Get sections for this doc
-                $sections_args = array(
+                // Get sections for this doc with limit
+                $sections_query_args = array(
                     'post_type' => 'docs',
                     'post_status' => 'publish',
-                    'posts_per_page' => $sections_per_doc === 'all' ? -1 : intval($sections_per_doc),
                     'post_parent' => $doc->ID,
                     'orderby' => 'menu_order',
-                    'order' => 'ASC'
+                    'order' => 'ASC',
+                    'posts_per_page' => $sections_per_doc === 'all' ? -1 : intval($sections_per_doc)
                 );
 
-                $sections = get_posts($sections_args);
+                $sections = get_posts($sections_query_args);
                 $total_articles = 0;
                 ?>
 
@@ -68,19 +75,20 @@
 
                     <?php if ($show_doc_article || !empty($sections)) : ?>
                         <?php
-                        // Calculate total articles
                         foreach ($sections as $section) {
-                            $articles_args = array(
+                            // Get articles for this section with limit
+                            $articles_query_args = array(
                                 'post_type' => 'docs',
                                 'post_status' => 'publish',
-                                'posts_per_page' => $articles_per_section === 'all' ? -1 : intval($articles_per_section),
                                 'post_parent' => $section->ID,
                                 'orderby' => 'menu_order',
-                                'order' => 'ASC'
+                                'order' => 'ASC',
+                                'posts_per_page' => $articles_per_section === 'all' ? -1 : intval($articles_per_section)
                             );
 
-                            $articles = get_posts($articles_args);
+                            $articles = get_posts($articles_query_args);
                             $total_articles += count($articles);
+
                             if (!$keep_articles_collapsed) : ?>
                                 <div class="wedocs-docs-grid__sections">
                                     <div class="wedocs-docs-grid__section">
@@ -104,9 +112,8 @@
                                 </div>
                             <?php endif;
                         }
-                        ?>
 
-                        <?php if ($show_doc_article) : ?>
+                        if ($show_doc_article) : ?>
                             <p class="wedocs-docs-grid__article-count">
                                 <?php printf(
                                     _n('%d article', '%d articles', $total_articles, 'wedocs'),
@@ -130,16 +137,40 @@
         <?php if ($enable_pagination && $total_pages > 1) : ?>
             <div class="wedocs-docs-pagination">
                 <?php
+                // Get the current page URL without pagination parameters
+                $pagenum_link = html_entity_decode(get_pagenum_link());
+                $query_args   = array();
+                $url_parts    = explode('?', $pagenum_link);
+
+                if (isset($url_parts[1])) {
+                    wp_parse_str($url_parts[1], $query_args);
+                }
+
+                // Remove any existing page parameters
+                unset($query_args['paged']);
+                unset($query_args['page']);
+
+                // Build the base URL
+                $pagenum_link = esc_url(add_query_arg($query_args, $url_parts[0]));
+
+                // Generate pagination links
                 echo paginate_links(array(
-                    'base' => get_pagenum_link(1) . '%_%',
-                    'format' => 'page/%#%',
+                    'base' => $pagenum_link . '%_%',
+                    'format' => '?page=%#%',
                     'current' => $current_page,
                     'total' => $total_pages,
                     'prev_text' => __('&laquo; Previous', 'wedocs'),
                     'next_text' => __('Next &raquo;', 'wedocs'),
-                    'type' => 'list'
+                    'type' => 'list',
+                    'add_args' => $query_args,
+                    'show_all' => false,
+                    'end_size' => 2,
+                    'mid_size' => 2
                 ));
                 ?>
             </div>
         <?php endif; ?>
     </div>
+    <?php
+    return ob_get_clean();
+}
