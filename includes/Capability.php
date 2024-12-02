4e075abc229347f4abcd7f400f7b86b3f7c84805
haideralibd <haider4uiu@gmail.com>
2024-12-02 12:50:36 +0600

    fix: translatepress infinite loop issue for checking capibility

--- includes/Capability.php
+++ includes/Capability.php
@@ -10,11 +10,11 @@ class Capability {
      * Initialize the class
      */
     public function __construct() {
-        add_filter( 'user_has_cap', [ $this, 'grant_delete_capability_to_specific_roles' ], 10, 4 );
+        add_filter( 'user_has_cap', array( $this, 'grant_delete_capability_to_specific_roles' ), 10, 4 );
     }
 
     /**
-	 * Grant doc delete capabilities to Admin, Editor and 
+	 * Grant doc delete capabilities to Admin, Editor and
 	 * weDocs-pro global permission given roles.
 	 *
 	 * @param array $all_caps An array of all the user's capabilities.
@@ -25,12 +25,21 @@ class Capability {
 	 * @return array
 	 */
     public function grant_delete_capability_to_specific_roles( $all_caps, $caps, $args, $user ) {
+        if ( ! isset( $_GET['post'] ) || 'docs' !== get_post_type( absint( $_GET['post'] ) ) ) {
+			return $all_caps;
+		}
+
         $permitted_roles = array( 'administrator', 'editor' );
-        $delete_caps = array(
+        $delete_caps     = array(
             'delete_published_docs' => true,
+			'delete_docs' => true,
             'delete_others_docs' => true,
             'delete_private_docs' => true,
         );
+
+		if ( empty( array_intersect( array_keys( $delete_caps ), $caps ) ) ) {
+            return $all_caps;
+        }
 
         if ( wedocs_pro_exists() ) {
             $permitted_roles = wedocs_get_permission_settings( 'global_permission', [ 'administrator', 'editor' ] );
