1af76e5a4ca254d925bd920effd253f192ca2422
Md. Aminur Islam <eng.aminurislam@gmail.com>
2024-12-05 13:02:38 +0600

    :rocket: [CHORE] version 2.1.10

--- wedocs.php
+++ wedocs.php
@@ -3,7 +3,7 @@
 Plugin Name: weDocs
 Plugin URI: https://wedocs.co/
 Description: A documentation plugin for WordPress
-Version: 2.1.9
+Version: 2.1.10
 Author: weDevs
 Author URI: https://wedocs.co/?utm_source=wporg&utm_medium=banner&utm_campaign=author-uri
 License: GPL2
@@ -60,7 +60,7 @@ final class WeDocs {
      *
      * @var string
      */
-    const VERSION = '2.1.9';
+    const VERSION = '2.1.10';
 
     /**
      * The plugin url.
