9c4b8b669d4403bbf5e8d02bb9eebe9f610a2e18
Iftakharul Islam Ifat <88052038+iftakharul-islam@users.noreply.github.com>
2025-04-28 10:33:03 +0600

    viewport support for block theme

--- templates/single-docs.php
+++ templates/single-docs.php
@@ -32,6 +32,7 @@ if (function_exists('wp_is_block_theme') && wp_is_block_theme()) {
     <html <?php language_attributes(); ?>>
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
