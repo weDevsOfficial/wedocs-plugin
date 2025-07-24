db56d724257bdd2e17f689e719e33b9886a3557c
Al Mahmud Alif <105235186+myself-alif@users.noreply.github.com>
2025-07-24 17:53:40 +0600

    contributors section removed (#214)
    * contributors section removed
    
    * removed packege json
    
    * additon: package-lock.json
    
    ---------
    
    Co-authored-by: weDevs <wedevs@weDevss-MacBook-Air.local>
    Co-authored-by: Al Mahmud Alif <wedevs@MacBookAir.localdomain>

--- assets/build/index.asset.php
+++ assets/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '527d9c316c5b0cf85f14');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7fd38c9fb0b998c4cff7');
