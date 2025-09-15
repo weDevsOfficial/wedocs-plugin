8fbba12f408583e0288396fb7715cf7ad568a2be
Sapayth Hossain <sapayth@users.noreply.github.com>
2025-09-15 10:05:21 +0600

    preview and block icon added

--- assets/build/block.asset.php
+++ assets/build/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e32b61959e64ec68b06e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '261dafb928ad68c1f5ab');
