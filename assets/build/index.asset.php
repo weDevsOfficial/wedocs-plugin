d829a7f0ac0b3edff681237df6a8803da5f979f4
Sapayth Hossain <sapayth@users.noreply.github.com>
2025-09-25 13:16:52 +0600

    align unwanted changes with develop

--- assets/build/index.asset.php
+++ assets/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '51d90c908944496c4646');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7fd38c9fb0b998c4cff7');
