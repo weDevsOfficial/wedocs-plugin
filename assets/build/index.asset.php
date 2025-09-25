a6772885d3b9ed15c438da2ac45da47f5621dce2
Iftakharul Islam Ifat <88052038+iftakharul-islam@users.noreply.github.com>
2025-09-25 19:11:55 +0600

    Updated shortcode doc link.

--- assets/build/index.asset.php
+++ assets/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7fd38c9fb0b998c4cff7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0de2a31d58bc4b4e787a');
