09ac607bd8ed55ba71e4edcc7e96d9ecd5f8f81c
Iftakharul Islam Ifat <88052038+iftakharul-islam@users.noreply.github.com>
2025-10-03 11:15:18 +0600

    update build file

--- assets/build/index.asset.php
+++ assets/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b6a80a87e12f638229fa');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b7c3a15c3078d8c568bf');
