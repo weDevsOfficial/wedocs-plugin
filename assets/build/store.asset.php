79eee0f9f29a5ed1666a8f71cf181c49ae5bd8d8
Iftakharul Islam Ifat <88052038+iftakharul-islam@users.noreply.github.com>
2025-10-22 12:48:36 +0600

    Fixed rest api endpoint error when wedocs pro is not active.

--- assets/build/store.asset.php
+++ assets/build/store.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-data'), 'version' => '15abe877e6e3ddc314f8');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-data'), 'version' => '9232fa4d737e9ba5aaf6');
