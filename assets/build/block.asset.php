0b945b676576221b65f5f4122138e542ebdec665
Sapayth Hossain <sapayth@users.noreply.github.com>
2025-11-13 15:33:40 +0600

    update ai icon and wording

--- assets/build/block.asset.php
+++ assets/build/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'aa2df97a4d78b70cd95a');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ab6c52188fae106c2903');
