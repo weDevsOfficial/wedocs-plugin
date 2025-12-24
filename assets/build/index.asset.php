6e3a08a12204fff7f6012d9309d4994ab9797ca4
Ariful Hoque <hoquea57@gmail.com>
2025-12-24 14:51:43 +0600

    Scope CSS resets to .wedocs-document class
    Updated the CSS reset rules in index.css to apply only within the .wedocs-document class, preventing global style overrides. Adjusted selectors throughout the stylesheet and added a new .w-max utility. Updated the asset version in index.asset.php and rebuilt index.js accordingly. Updated translation template wedocs.pot.

--- assets/build/index.asset.php
+++ assets/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '1fd015210a7390e3647a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ab6e63c54006ff4e8837');
