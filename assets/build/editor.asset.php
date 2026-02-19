880d8adf93029f1de6d8b6f95bafbed46caf1c06
Iftakharul Islam Ifat <88052038+iftakharul-islam@users.noreply.github.com>
2026-02-19 14:15:59 +0600

    update build

---
+++ assets/build/editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '616890f0f10e270fbdb3');
---
+++ assets/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '00dafafa50d7dff9c144');
