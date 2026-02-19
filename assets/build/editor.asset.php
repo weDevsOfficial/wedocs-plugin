8457655cf0d3469f365d49cbf0d02de74124aa22
Iftakharul Islam Ifat <88052038+iftakharul-islam@users.noreply.github.com>
2026-02-19 14:10:23 +0600

    Delete assets/build/editor.asset.php

--- assets/build/editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'cd242c4d68f4cf18cbcc');
